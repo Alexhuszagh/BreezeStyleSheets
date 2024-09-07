639c2a083fbea20a0da127ba71aff55bb2aee350
Alex Huszagh <ahuszagh@gmail.com>
2024-09-07 08:07:10 -0500

    Add script to configure for all frameworks.

---
+++ ci/configure_all.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#
+# Run each configure for all supported frameworks, and store them in `dist/ci`.
+#
+
+set -eux pipefail
+
+ci_home="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+project_home="$(dirname "${ci_home}")"
+mkdir -p "${project_home}/dist/ci"
+cd "${project_home}"
+
+# pop them into dist since it's ignored anyway
+if [[ ! -v PYTHON ]]; then
+    PYTHON=python
+fi
+frameworks=("pyqt5" "pyqt6" "pyside6")
+have_pyside=$(PYTHON -c 'import sys; print(sys.version_info < (3, 11))')
+if [[ "${have_pyside}" == "True" ]]; then
+    frameworks+=("pyside2")
+fi
+
+# NOTE: We need to make sure the scripts directory is added to the path
+python_home=$(PYTHON -c 'import site; print(site.getsitepackages()[0])')
+scripts_dir="${python_home}/scripts"
+uname_s="$(uname -s)"
+if [[ "${uname_s}" == MINGW* ]]; then
+    # want to convert C:/... to /c/...
+    scripts_dir=$(echo "/$scripts_dir" | sed -e 's/\\/\//g' -e 's/://')
+fi
+export PATH="${scripts_dir}:${PATH}"
+for framework in "${frameworks[@]}"; do
+    "${PYTHON}" "${project_home}/configure.py" \
+        --styles=all \
+        --extensions=all \
+        --qt-framework "${framework}" \
+        --output-dir "${project_home}/dist/ci" \
+        --resource "breeze_${framework}.qrc" \
+        --compiled-resource "${project_home}/dist/ci/breeze_${framework}.py"
+    # this will auto-fail due to pipefail, checks the imports work
+    "${PYTHON}" -c "import os; os.chdir('dist/ci'); import breeze_${framework}"
+done
