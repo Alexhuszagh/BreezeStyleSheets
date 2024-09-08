d5e5a707db6ab8ab4c3173e2705adb70c7cef216
Alex Huszagh <ahuszagh@gmail.com>
2024-09-07 22:55:13 -0500

    Allow optional overrides for Python in scripts.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,10 +8,18 @@ set -eux pipefail
 scripts_home="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 project_home="$(dirname "${scripts_home}")"
 cd "${project_home}"
+# shellcheck source=/dev/null
+. "${scripts_home}/shared.sh"
 
 # run our python lint checks
-pylint ./*.py example/*.py example/**/*.py
-pyright example/breeze_theme.py
-flake8
-
-# run our C++ lint checks
+# unless we manually provide an override, use whatever's
+# on the path by default.
+if ! is-set PYTHON; then
+    pylint ./*.py example/*.py example/**/*.py
+    pyright example/breeze_theme.py
+    flake8
+else
+    ${PYTHON} -m pylint ./*.py example/*.py example/**/*.py
+    ${PYTHON} -m pyright example/breeze_theme.py
+    ${PYTHON} -m flake8
+fi
