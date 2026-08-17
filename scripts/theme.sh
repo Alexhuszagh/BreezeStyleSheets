ecc29169fc8f98a0a279a0bf336c72453c8875d5
Alex Huszagh <ahuszagh@gmail.com>
2026-08-17 18:13:34 -0500

    Patch validating the theme detection.

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -14,7 +14,7 @@ fi
 # Check the import first, then calling the function for easier debugging.
 ${PYTHON} -c "import breezestylesheets"
 theme=$(${PYTHON} -c "from breezestylesheets import detect; print(detect.get_theme())")
-if [[ ! "${theme}" =~ ^[0-9]+$ ]]; then
+if [[ ! "${theme}" =~ ^[0-9]+$ ]] && [[ "${theme}" != SystemTheme.* ]]; then
     >&2 echo "Unable to get the correct theme."
     exit 1
 fi
